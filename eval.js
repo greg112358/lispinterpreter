4ed51b3096fa5af954c80418bae47e77f4c2b748
Gregory Walker <greg112358@hotmail.com>
2023-07-11 20:38:10 -0700

    forgot eval

--- eval.js
+++ eval.js
@@ -1,9 +1,9 @@
-const {TYPES,CAR,CDR,CONS,PRIMITIVE_FUNCTIONS} = require("./primitives");
+const { TYPES, CAR, CDR, CONS, PRIMITIVE_FUNCTIONS } = require("./primitives");
 
 function EVAL(exp, env) {
     var car = CAR(exp);
     var cdr = CDR(exp);
-    if(car == null){
+    if (car == null) {
         return null;
     }
     if (car.type === TYPES.NUMBER || car.type === TYPES.STRING) {
@@ -15,33 +15,33 @@ function EVAL(exp, env) {
 
 
 
-function APPLY(fn, args,env) {
-    if(PRIMITIVE_FUNCTIONS.includes(fn.value)){
-        return APPLY_PRIMITIVE(fn,EVAL_LIST(args),env);
+function APPLY(fn, args, env) {
+    if (PRIMITIVE_FUNCTIONS.includes(fn.value)) {
+        return APPLY_PRIMITIVE(fn, EVAL_LIST(args), env);
     }
 }
 
-function APPLY_PRIMITIVE(fn,args,env){
+function APPLY_PRIMITIVE(fn, args, env) {
 
-    if(fn.value==="*"){
-        var car  = CAR(args);
+    if (fn.value === "*") {
+        var car = CAR(args);
         var cdr = CDR(args);
-        if(car == null && cdr == null){
+        if (car == null && cdr == null) {
             return car.value * cdr.value
         }
 
-    }else if((fn.value==="+")){
-        
+    } else if ((fn.value === "+")) {
+
     }
 }
 
 
 
-function EVAL_LIST(list,env){
-    if(list==null){
+function EVAL_LIST(list, env) {
+    if (list == null) {
         return null;
     }
-    return CONS(EVAL(CAR(list),env),EVAL_LIST(CDR(list),env));
+    return CONS(EVAL(CAR(list), env), EVAL_LIST(CDR(list), env));
 }
 
 
